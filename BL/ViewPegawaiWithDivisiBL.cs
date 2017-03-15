431c96cacecb1aae41d4a7803efc58ef88c2f5be
erick <erick@DESKTOP-7DP9Q9N>
2017-03-15 09:20:52 +0700

    menambahkan api untuk mengambil ViewPegawaiWithDivisi

---
+++ BL/ViewPegawaiWithDivisiBL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BO;
+using DAL;
+
+namespace BL
+{
+    public class ViewPegawaiWithDivisiBL
+    {
+        public IEnumerable<ViewPegawaiWithDivisi> GetAllData()
+        {
+            ViewPegawaiWithDivisiDAL viewDal = new ViewPegawaiWithDivisiDAL();
+            return viewDal.GetAllData();
+        }
+    }
+}
---
+++ SampleAPI/Controllers/ViewPegawaiWithDivisiController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using BO;
+using BL;
+
+namespace SampleAPI.Controllers
+{
+    public class ViewPegawaiWithDivisiController : ApiController
+    {
+        // GET: api/ViewPegawaiWithDivisi
+        public IEnumerable<ViewPegawaiWithDivisi> Get()
+        {
+            ViewPegawaiWithDivisiBL viewBL = new ViewPegawaiWithDivisiBL();
+            return viewBL.GetAllData();
+        }
+
+        // GET: api/ViewPegawaiWithDivisi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ViewPegawaiWithDivisi
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/ViewPegawaiWithDivisi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ViewPegawaiWithDivisi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
